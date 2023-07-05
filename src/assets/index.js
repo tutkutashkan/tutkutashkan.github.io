64a93f7f9e8687dc1474fb9a3d8a13b5e7c50f4b
tutkutashkan <72821963+tutkutashkan@users.noreply.github.com>
2023-07-06 02:11:05 +0300

    Add files via upload

---
+++ src/assets/index.js
@@ -0,0 +1,82 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+import csharp from "./tech/csharp.png";
+import java from "./tech/java.png";
+import python from "./tech/python.png";
+import sql from "./tech/sql.png";
+import rlogo from "./tech/rlogo.png";
+import swift from "./tech/swift.png";
+import haskell from "./tech/haskell.png";
+
+import bytecomputer from "./company/byte.png";
+import bagep from "./company/bagep.png";
+import freelance from "./company/freelance.png";
+
+import doga from "./company/doga.png";
+import uol from "./company/uol.png";
+
+import asteroid from "./asteroid.png";
+import comp390 from "./comp390.png";
+import sidescroll from "./sidescroll.png";
+import sqlproj from "./sqlproj.png";
+import maskrcnn from "./maskrcnn.png";
+import mapof from "./mapof.png";
+
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  csharp,
+  java,
+  python,
+  sql,
+  rlogo,
+  swift,
+  haskell,
+  bytecomputer,
+  bagep,
+  freelance,
+  doga,
+  uol,
+  asteroid,
+  comp390,
+  sidescroll,
+  sqlproj,
+  maskrcnn,
+  mapof,
+};
---
+++ src/components/index.js
@@ -0,0 +1,26 @@
+import { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas } from './canvas';
+import Hero from './Hero';
+import Navbar from './Navbar';
+import About from './About';
+import Tech from './Tech';
+import Experience from './Experience';
+import Education from './Education';
+import Works from './Works';
+import Feedbacks from './Feedbacks';
+import Contact from './Contact';
+
+export {
+  Hero,
+  Navbar,
+  About,
+  Tech,
+  Experience,
+  Education,
+  Works,
+  Feedbacks,
+  Contact,
+  EarthCanvas, 
+  BallCanvas, 
+  ComputersCanvas, 
+  StarsCanvas
+}
